ab0e3f87a0d4c521c234bbf9cf597dafa841cd44
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-09-25 20:59:34 +0530

    Create Hello World Rest Controller

---
+++ d1/hello_webservice/src/main/java/lk/ijse/webservices/helloworld/rest/HelloWorldSimpleRest.java
@@ -0,0 +1,22 @@
+package lk.ijse.webservices.helloworld.rest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorldSimpleRest extends HttpServlet {
+
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{ \"hello\": \"World\"}");
+    }
+
+
+}
