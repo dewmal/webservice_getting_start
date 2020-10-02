ed283c44c5eb3c825ca44efabbda551de5e0e37c
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-02 22:11:58 +0530

    Show message object as json

--- d2/src/main/java/lk/ijse/webservice/resource_access/api/ResourceAccessRest.java
+++ d2/src/main/java/lk/ijse/webservice/resource_access/api/ResourceAccessRest.java
@@ -1,10 +1,14 @@
 package lk.ijse.webservice.resource_access.api;
 
+import com.google.gson.Gson;
+import lk.ijse.webservice.resource_access.modal.Message;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 public class ResourceAccessRest extends HttpServlet {
 
@@ -13,9 +17,14 @@ public class ResourceAccessRest extends HttpServlet {
             HttpServletResponse response)
             throws ServletException, IOException {
 
+        Message message = new Message("Test", "Message", new Date());
+        Gson gson = new Gson();
+        String json = gson.toJson(message);
+
+
         response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().println("{ \"hello\": \"World\"}");
+        response.getWriter().println(json);
     }
 
 }
