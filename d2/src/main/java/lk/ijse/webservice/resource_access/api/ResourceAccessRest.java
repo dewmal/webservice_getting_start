bc1845c14859c03026b02ef7d8fc627460843f85
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-02 22:03:15 +0530

    basic server features

---
+++ d2/src/main/java/lk/ijse/webservice/resource_access/api/ResourceAccessRest.java
@@ -0,0 +1,21 @@
+package lk.ijse.webservice.resource_access.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResourceAccessRest extends HttpServlet {
+
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{ \"hello\": \"World\"}");
+    }
+
+}
