de20f2e14d43d88c01374dc7772a4f18fd267591
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-16 23:47:59 +0530

    Day 3 how to deploy the web service into aws beanstalk

--- d3/aws_beanstalk/src/main/java/lk/ijse/webservice/resource_access/RestServer.java
+++ d3/aws_beanstalk/src/main/java/lk/ijse/webservice/resource_access/RestServer.java
@@ -9,10 +9,14 @@ import org.eclipse.jetty.servlet.ServletHandler;
 public class RestServer {
     private Server server;
 
+    private static int getPort() {
+        return Integer.parseInt(System.getenv().getOrDefault("PORT", "5000"));
+    }
+
     public void start() throws Exception {
         server = new Server();
         ServerConnector connector = new ServerConnector(server);
-        connector.setPort(8090);
+        connector.setPort(getPort());
         server.setConnectors(new Connector[]{connector});
 
         ServletHandler servletHandler = new ServletHandler();
