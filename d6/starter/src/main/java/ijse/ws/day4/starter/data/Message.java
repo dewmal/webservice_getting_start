5cda015404e77e0e3f8bbd2408a180758c364145
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-10-31 16:12:31 +0530

    Create Message POJO

---
+++ d6/starter/src/main/java/ijse/ws/day4/starter/data/Message.java
@@ -0,0 +1,33 @@
+package ijse.ws.day4.starter.data;
+
+import java.util.Date;
+
+public class Message {
+    private String sender;
+    private String message;
+    private Date sentTime;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getSentTime() {
+        return sentTime;
+    }
+
+    public void setSentTime(Date sentTime) {
+        this.sentTime = sentTime;
+    }
+}
