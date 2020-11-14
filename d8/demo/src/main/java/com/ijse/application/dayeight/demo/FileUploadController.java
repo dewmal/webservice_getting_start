6b60b0c1a7c4f2b67262bcfd1bc9c7f3578ec57c
Dewmal Nilanka <dewmalnilanka@gmail.com>
2020-11-14 16:58:50 +0530

    File upload using spring boot rest controller

---
+++ d8/demo/src/main/java/com/ijse/application/dayeight/demo/FileUploadController.java
@@ -0,0 +1,32 @@
+package com.ijse.application.dayeight.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+
+@Controller
+public class FileUploadController {
+
+    @GetMapping("/api/upload")
+    @ResponseBody
+    public String uploadedData() {
+        return "DONE";
+    }
+
+    @PostMapping("/api/upload")
+    @ResponseBody
+    public String uploadData(@RequestParam("file") MultipartFile file) {
+        System.out.println(file);
+        File uploadedFile = new File("text_uploaded");
+
+        try (OutputStream os = new FileOutputStream(uploadedFile)) {
+            os.write(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "DONE";
+    }
+}
